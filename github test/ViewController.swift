32875933f7fdebe90e5d39cefef80716e2601e6d
Liam Driver (s5111108) <s5111108@mac38567.mac.bournemouth.ac.uk>
2019-04-02 11:33:42 +0100

    Initial Commit

---
+++ github test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github test
+//
+//  Created by Liam Driver (s5111108) on 02/04/2019.
+//  Copyright © 2019 Liam Driver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
